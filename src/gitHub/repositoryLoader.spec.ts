161cecdcaae99fdb3d882cda177df31bfb449013
Alexander Rose <alexander.rose@codecentric.de>
2024-01-02 13:28:05 +0100

    test(repository-loader): load repositories

--- src/gitHub/repositoryLoader.spec.ts
+++ src/gitHub/repositoryLoader.spec.ts
@@ -1,5 +1,19 @@
+import { loadPopularRepositories } from "./repositoryLoader";
+
 describe("loadPopularRepositories", () => {
   beforeEach(() => {
     jest.clearAllMocks();
+  });
+
+  it("makes an HTTP call to GitHub", async () => {
+    jest.spyOn(global, "fetch").mockResolvedValue({
+      json: async () => [],
+    } as Response);
+
+    await loadPopularRepositories();
+
+    expect(global.fetch).toHaveBeenCalledWith(
+      expect.stringContaining("https://api.github.com/search/repositories"),
+    );
   });
 });
