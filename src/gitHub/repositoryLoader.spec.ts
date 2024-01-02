e438b8c2643c61807eb51e835032935f5901c2cd
Alexander Rose <alexander.rose@codecentric.de>
2024-01-02 13:29:56 +0100

    test(repository-loader): return empty array

--- src/gitHub/repositoryLoader.spec.ts
+++ src/gitHub/repositoryLoader.spec.ts
@@ -16,4 +16,14 @@ describe("loadPopularRepositories", () => {
       expect.stringContaining("https://api.github.com/search/repositories"),
     );
   });
+
+  it("returns an empty array if the response is empty", async () => {
+    jest.spyOn(global, "fetch").mockResolvedValue({
+      json: async () => [],
+    } as Response);
+
+    const repositories = await loadPopularRepositories();
+
+    expect(repositories).toEqual([]);
+  });
 });
