2ad21e2d35757fdec9ed62689cd73808d237c90e
Alexander Rose <alexander.rose@codecentric.de>
2024-01-02 12:55:21 +0100

    chore(express): initialize express boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("WIP");
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port = process.env.PORT ?? 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Dummy route");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
